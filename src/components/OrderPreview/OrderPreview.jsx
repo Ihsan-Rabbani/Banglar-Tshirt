269d9f2f784560a741004fa6e5b84896887cae7d
Ihsan-Rabbani <ihrbbn@gmail.com>
2023-04-07 06:40:21 +0600

    Tshirt length shown

---
+++ src/components/OrderPreview/OrderPreview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderPreview = () => {
+    return (
+        <div>
+            <h2>This is Order Preview</h2>
+        </div>
+    );
+};
+
+export default OrderPreview;
